7c89d8696eae99d7e2e8796e89a98e9b58b72509
darryl chandra <darrylchan05@gmail.com>
2025-05-14 09:19:10 +0700

    fixed the route

--- backend/index.js
+++ backend/index.js
@@ -54,7 +54,7 @@ app.get('/gemini-explanation', controller.generateGemini)
 app.get('/mission', controller.getMission);
 app.post('/auth/mission', controller.postMission);
 app.delete('/auth/mission', controller.deleteMission);
-app.post("mission/progress", controller.postUserMissionProgress);
+app.post("/mission/progress", controller.postUserMissionProgress);
 app.post('/google-signin', controller.postGoogleSignIn);
 // Use PORT from env or default to 3000
 const PORT = 3001;
