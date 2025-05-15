976f14b3d27c362b48ea3689e4feb594d4f13585
KiyuZo <ramaandikapratama4@gmail.com>
2025-05-15 16:15:35 +0700

    change the domain on login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -46,7 +46,7 @@ const LoginPage = () => {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://localhost:3001/login', {
+      const response = await fetch('http://project-adams.vercel.app/3001/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -102,7 +102,7 @@ const LoginPage = () => {
     }
 
     try {
-      await fetch('http://localhost:3001/google-signin', {
+      await fetch('http://project-adams.vercel.app/3001/google-signin', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
